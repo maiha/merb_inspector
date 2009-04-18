f165142868484a54eb8ed6737f9a9b37047f76b4
maiha <maiha@wota.jp>
2009-04-18 17:26:00 +0900

    validate keys in inspect helper

--- lib/merb_inspector/helper.rb
+++ lib/merb_inspector/helper.rb
@@ -6,10 +6,21 @@ module Merb
         object  = args.shift
         options = args.shift || {}
 
-        options = options.is_a?(Hash) ? options : {:action=>options}
+        unless options.is_a?(Hash)
+          action = options.to_s
+          options = args.shift || {}
+          options[:action] = action
+        end
         options[:action]    ||= :show
         options[:level]     ||= 1
         options[:max_level] ||= 3
+
+        # validate keys
+        valid_keys   = [:action, :level, :max_level]
+        unknown_keys = options.keys - valid_keys
+        if !unknown_keys.blank?
+          return h("[INSPECTOR ERROR] Unknown key(s): %s" % unknown_keys.inspect)
+        end
 
         inspector_class = BasicInspector if options[:level] >= options[:max_level]
         inspector_class ||= Manager.lookup(object) || Merb::Inspector.default
